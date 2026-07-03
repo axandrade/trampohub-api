60b5d757b016df4d6f51e316a0103d5f263147c8
Alexsandro Andrade <alexsandro.mpe@gmail.com>
2026-07-03 13:39:05 -0300

    fix: reorder import statements in views.py for clarity

--- vagas/views.py
+++ vagas/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, status
+from rest_framework import status
+from rest_framework_mongoengine import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
