e2ff9309c0965726df66caf7f2abd074062856b6
Alexsandro Andrade <alexsandro.mpe@gmail.com>
2026-07-06 08:50:58 -0300

    feat: update settings to use environment variable for SECRET_KEY and add .env to .gitignore

--- trampohub_api/settings.py
+++ trampohub_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ax2!(0$*9_5_uww_pmf9r3z7ghpdw68)j@3*ipp*=fi133#l3x'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
